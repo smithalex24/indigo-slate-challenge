04537a442f1417e75dce8400ae55e1122a6e062c
Alex Smith <smith25243@gmail.com>
2018-08-23 13:39:12 -0700

    finished menu items and added more styling

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-$('.nav-toggle').click(function(e) {
+$(document).ready(function(e) {
+	$('.nav-toggle').click(function(e) {
   
-  e.preventDefault();
-  $("html").toggleClass("openNav");
-  $(".nav-toggle").toggleClass("active");
+  	e.preventDefault();
+  	$("html").toggleClass("openNav");
+  	$(".nav-toggle").toggleClass("active");
 
 });
+	$("#welcomeHeader").addClass('animated fadeIn');
+	$("#welcomeSubHeader").addClass('animated fadeIn');
+
+})
